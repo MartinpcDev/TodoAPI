be15a79f45990631bba08272ad84a7f96417f703
MartinPC <MartinChrisPC@hotmail.com>
2024-11-06 18:14:20 -0500

    feat: agregando jwtfilter

--- src/main/java/com/martin/api/config/JwtAuthenticationFilter.java
+++ src/main/java/com/martin/api/config/JwtAuthenticationFilter.java
@@ -1,4 +1,60 @@
 package com.martin.api.config;
 
-public class JwtAuthenticationFilter {
+import com.martin.api.exception.AuthNotValidException;
+import com.martin.api.persistence.repository.UserRepository;
+import com.martin.api.service.IJwtService;
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final IJwtService jwtService;
+  private final UserRepository userRepository;
+  private final UserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(
+      @Nonnull HttpServletRequest request,
+      @Nonnull HttpServletResponse response,
+      @Nonnull FilterChain filterChain) throws ServletException, IOException {
+
+    String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      String token = authHeader.substring(7);
+      String username = jwtService.extractUsername(token);
+      userRepository.findByEmailIgnoreCase(username).ifPresentOrElse(user -> {
+        if (jwtService.isTokenValid(token, user)) {
+          UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+          UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+              userDetails,
+              null,
+              userDetails.getAuthorities()
+          );
+          authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(authToken);
+        } else {
+          throw new AuthNotValidException("Token invalido");
+        }
+      }, () -> {
+        throw new AuthNotValidException("El usuario no existe");
+      });
+    }
+
+    filterChain.doFilter(request, response);
+  }
 }
